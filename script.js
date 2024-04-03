392e06ff06862db3815cbd90a05c2c271b9f2779
Subham9812 <subhamdev79@gmail.com>
2024-04-03 23:44:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let hr = document.getElementById('hr');
+let mn = document.getElementById('mn')
+let sc = document.getElementById('sc');
+
+function displayTime() {
+    let day = new Date();
+
+    let hh = day.getHours();
+    let mm = day.getMinutes();
+    let ss = day.getSeconds();
+
+    let hou = hh*30 + mm/2;
+    let mi = mm * 6;
+    let se = ss * 6;
+
+    hr.style.transform = `rotate(${hou}deg)`;
+    mn.style.transform = `rotate(${mi}deg)`;
+    sc.style.transform = `rotate(${se}deg)`;
+
+    // Digital Clock
+    let hour = document.getElementById('hour');
+    let min = document.getElementById('minutes');
+    let sec = document.getElementById('second');
+    let ampm = document.getElementById('ampm');
+
+    // let date = new Date();
+    let Dhh = new Date().getHours();
+    let Dmm = new Date().getMinutes();
+    let Dss = new Date().getSeconds();
+
+    // condition to change am to pm
+    let am = Dhh >= 12 ? "PM" : "AM";
+
+    // convert 24hr clock to 12hr clock
+    if(Dhh > 12){
+        Dhh = Dhh - 12;
+    }
+
+    // add zero before single digit number
+    Dhh = (Dhh < 10) ? "0" + Dhh : Dhh;
+    Dmm = (Dmm < 10) ? "0" + Dmm : Dmm;
+    Dss = (Dss < 10) ? "0" + Dss : Dss;
+
+    hour.innerHTML = Dhh;
+    min.innerHTML = Dmm;
+    sec.innerHTML = Dss;
+    ampm.innerHTML = am;
+}
+setInterval(displayTime, 1000);
